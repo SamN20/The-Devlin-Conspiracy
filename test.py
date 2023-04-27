f6d545353edb33a5b8c7751e44c30ea243567a28
SamN20 <nolans018@gmail.com>
2023-04-27 09:21:36 -0400

    proving Jonah Wrong

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 from cmu_graphics import * 
 
+
+# [attack distance, player speed, ]
+xpModifyers = {
+    '5': [30, ]
+
+
+
+
+}
+
 def onKeyPress(key, modifiers):
     print(key)
     print(modifiers)
+
+
+
+    # rdyguhijkl
 
 cmu_graphics.run()
