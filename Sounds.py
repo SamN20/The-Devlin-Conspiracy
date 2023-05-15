c1081907481ae7bf437dcdb6ba49d9386681f753
SamN20 <69125765+SamN20@users.noreply.github.com>
2023-05-14 22:52:37 -0400

    Parts of merge that didnt go thru

--- Sounds.py
+++ Sounds.py
@@ -3,6 +3,7 @@ from cmu_graphics import *
 # Soundtrack 
 Titlescreen = Sound('Sound_files\SoundTrack\David Baron-Burning Sun-Master--137.00bpm-Fm.wav')
 breakStelth = Sound('Sound_files\SoundTrack\TheDGTL-Hustle Like Dat-Master--88.00bpm-Ebm.wav')
+Tutorial = Sound('Sound_files\SoundTrack\ALIBI_Music-Catatumbo_Lightning-Master--130.00bpm-F.wav')
 
 # Sound Effects
 
